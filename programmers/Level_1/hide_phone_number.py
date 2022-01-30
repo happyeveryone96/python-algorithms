84dc70283c1f19c1f7f65596caea1334ed48476f
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-01-30 20:39:06 +0900

    핸드폰 번호 가리기

---
+++ programmers/Level_1/hide_phone_number.py
@@ -0,0 +1,10 @@
+def solution(phone_number):
+    result = []
+    num_len = len(phone_number)
+    for i in range(num_len):
+        if (i < num_len-4):
+            result.append('*')
+        else:
+            result.append(phone_number[i])
+    answer = ''.join(result)
+    return answer
