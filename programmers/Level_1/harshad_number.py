af660e8d7dffd06e8a9f0beb9e67b20acbf6d6be
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-01-31 20:04:11 +0900

    하샤드 수

---
+++ programmers/Level_1/harshad_number.py
@@ -0,0 +1,10 @@
+def solution(x):
+    str_num = str(x)
+    sum = 0
+    for i in range(len(str_num)):
+        sum += int(str_num[i])
+    if (x % sum == 0):
+        answer = True
+    else:
+        answer = False
+    return answer
