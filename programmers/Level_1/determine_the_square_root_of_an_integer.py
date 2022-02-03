3193f766528fbd6d0d834109a31b93d0391aa5bd
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-02-03 21:36:39 +0900

    정수 제곱근 판별

--- programmers/Level_1/determine_the_square_root_of_an_integer.py
+++ programmers/Level_1/determine_the_square_root_of_an_integer.py
@@ -2,7 +2,7 @@ import math
 
 
 def solution(n):
-    if (float.is_integer(math.sqrt(n))):
+    if float.is_integer(math.sqrt(n)):
         x = math.sqrt(n)
         answer = (x+1) * (x+1)
     else:
