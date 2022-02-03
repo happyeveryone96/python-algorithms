f6be1ae25a7441a520cf121c676c7872520cb222
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-02-03 21:38:28 +0900

    하샤드 수

--- programmers/Level_1/harshad_number.py
+++ programmers/Level_1/harshad_number.py
@@ -3,7 +3,7 @@ def solution(x):
     sum = 0
     for i in range(len(str_num)):
         sum += int(str_num[i])
-    if (x % sum == 0):
+    if x % sum == 0:
         answer = True
     else:
         answer = False
