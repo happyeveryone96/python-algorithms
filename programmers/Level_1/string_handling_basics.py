3b887eac8f9eaaa6f1b827f497ed1e6ecd615f60
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-02-03 21:13:42 +0900

    문자열 다루기 기본

---
+++ programmers/Level_1/string_handling_basics.py
@@ -0,0 +1,9 @@
+def solution(s):
+    if (s.isdigit() == True and (len(s) == 4 or len(s) == 6)):
+        answer = True
+        for i in range(len(s)):
+            if (s[i] == 'e'):
+                answer = False
+    else:
+        answer = False
+    return answer
