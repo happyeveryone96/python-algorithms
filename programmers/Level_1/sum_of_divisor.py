0c3ddaaffc47ac0e865d35aa7284b1492739e0b5
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-02-03 21:43:22 +0900

    약수의 합

--- programmers/Level_1/sum_of_divisor.py
+++ programmers/Level_1/sum_of_divisor.py
@@ -1,6 +1,6 @@
 def solution(n):
     answer = 0
     for i in range(n+1):
-        if (i != 0 and float.is_integer(n / i) == True):
+        if i != 0 and float.is_integer(n / i) == True:
             answer += i
     return answer
