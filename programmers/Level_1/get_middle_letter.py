7fafed5ef1b3fa609367012be52b9a20c33e682f
happyeveryone96 <66675699+happyeveryone96@users.noreply.github.com>
2022-02-03 21:38:04 +0900

    가운데 글자 가져오기

--- programmers/Level_1/get_middle_letter.py
+++ programmers/Level_1/get_middle_letter.py
@@ -1,6 +1,6 @@
 def solution(s):
     answer = []
-    if (len(s) % 2 == 0):
+    if len(s) % 2 == 0:
         answer.extend([s[int((len(s))/2-1)], s[int((len(s))/2)]])
     else:
         answer.append(s[int((len(s)-1)/2)])
